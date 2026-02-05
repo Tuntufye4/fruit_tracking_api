d1e8ef751fe70d8ee308738c58ba8e2afff9b061
Tuntufye4 <tuntumwanyongo@gmail.com>
2026-02-05 16:38:13 +0200

    added render url

--- fruit_tracker/settings.py
+++ fruit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j7qg^ydj9%1!u_5ebd%9z5@$3psz6(z*681!-ou9*ju!ditki&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://fruit-tracking-api.onrender.com/']       
 
 
 # Application definition   
